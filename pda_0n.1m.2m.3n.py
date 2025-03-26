4864b13626118cc82a07e87c00369a7d35a1a8b0
Bhavana725 <128887317+Bhavana725@users.noreply.github.com>
2025-03-26 10:54:00 +0530

    Update pda_0n.1m.2m.3n.py

--- pda_0n.1m.2m.3n.py
+++ pda_0n.1m.2m.3n.py
@@ -1,80 +1,59 @@
-class PDA:
-    def __init__(self):
-        self.stack = [] 
-        self.state = 'q0' 
-        self.i = 0
-        
-    def reset(self):
-        self.stack = []  
-        self.state = 'q0'  
-        self.i = 0
-        
-    def process_input(self, input_string):
-        self.stack.append('Z0')
-        
-        for symbol in input_string:
-            if self.state == 'q0':  
-                if symbol == '0':
-                    self.stack.append('0')
-                elif symbol == '1':
-                    self.stack.append('1')
-                    self.state = 'q1'  
-                else:
-                    return False  
-            elif self.state == 'q1': 
-                if symbol == '1':
-                    self.stack.append('1')  
-                elif symbol == '2'and self.stack[-1] == '1':
-                    self.state = 'q2'  
-                    self.stack.pop()
-                else:
-                    return False  
-                    
-            elif self.state == 'q2':  
-                if symbol == '2':
-                    if self.stack and self.stack[-1] == '1':  
-                        self.stack.pop()
-                    else:
-                        return False  
-                elif symbol == '3' and self.stack[-1] == '0':  
-                    self.stack.pop()
-                    self.state = 'q3' 
-                else:
-                    return False
-                    
-            elif self.state == 'q3':  
-                if symbol == '3':
-                    if self.stack and self.stack[-1] == '0':  
-                        self.stack.pop()
-                    else:
-                        return False 
-                else:
-                    return False 
+def is_accepted_by_pda(string):
+    stack1 = []
+    stack2 = []
+    stage = 1
 
-        if self.stack == ['Z0']:
-            return True  
-        else:
-            return False  
-            
-def test_pda():
-    pda = PDA()
+    for symbol in string:
+        if stage == 1:
+            if symbol == '0':
+                stack1.append('0')
+            elif symbol == '1':
+                stack2.append('1')
+                stage = 2
+            else:
+                return False
 
-    # Test cases
-    test_strings = [
-        "011223",
-        "011233",   
-        "012233",   
-        "00112233", 
-        "011223",   
-        "0001112233", 
-        "0123"
-    ]
+        elif stage == 2:
+            if symbol == '1':
+                stack2.append('1')
+            elif symbol == '2':
+                if stack2:
+                    stack2.pop()
+                else:
+                    return False
+                stage = 3
+            else:
+                return False
 
-    for test_string in test_strings:
-        pda.reset()  
-        result = pda.process_input(test_string)
-        print(f"Input: {test_string} - {'Accepted' if result else 'Rejected'}")
+        elif stage == 3:
+            if symbol == '2':
+                if stack2:
+                    stack2.pop()
+                else:
+                    return False
+            elif symbol == '3':
+                if stack1:
+                    stack1.pop()
+                else:
+                    return False
+                stage = 4
+            else:
+                return False
 
-# Run the test
-if __name__ == "__main__":
-    test_pda()
+        elif stage == 4:
+            if symbol == '3':
+                if stack1:
+                    stack1.pop()
+                else:
+                    return False
+            else:
+                return False
+
+    return len(stack1) == 0 and len(stack2) == 0
+
+input_string = input("Enter a string: ")
+
+if is_accepted_by_pda(input_string):
+    print("String is accepted.")
+else:
+    print("String is not accepted.")

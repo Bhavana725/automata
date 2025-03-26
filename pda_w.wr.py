872682bee3182a4dddc444c4bda337cf3d0113cd
Bhavana725 <128887317+Bhavana725@users.noreply.github.com>
2025-03-26 10:55:30 +0530

    Update pda_w.wr.py

--- pda_w.wr.py
+++ pda_w.wr.py
@@ -1,23 +1,20 @@
 def is_accepted_by_pda(string):
     stack = []
-    found_C = False
-    index = 0
+    length = len(string)
 
-    while index < len(string):
-        char = string[index]
+    if length % 2 != 0:
+        return False  
 
-        if not found_C:
-            if char == 'C':
-                found_C = True
-            else:
-                stack.append(char)
-        else:
-            if not stack or stack.pop() != char:
-                return False
+    mid = length // 2
 
-        index += 1
+    for i in range(mid):
+        stack.append(string[i])
 
-    return len(stack) == 0 and found_C
+    for i in range(mid, length):
+        if not stack or stack.pop() != string[i]:
+            return False  
+
+    return len(stack) == 0
 
 input_string = input("Enter a string: ")
 
